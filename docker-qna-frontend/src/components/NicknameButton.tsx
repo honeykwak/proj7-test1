a67e48262a35f0c0eb131488dd8e6964dfa7c07a
honeykwak <jenilove0517@naver.com>
2024-05-23 12:02:02 +0900

    change text

--- docker-qna-frontend/src/components/NicknameButton.tsx
+++ docker-qna-frontend/src/components/NicknameButton.tsx
@@ -36,7 +36,7 @@ const NicknameButton = () => {
     <>
       {isNickname ? (
         <Button onPress={onOpen} color="primary" href="#" variant="flat">
-          질문하기
+          질문할래
         </Button>
       ) : (
         <Chip size="lg" color="primary" variant="flat">
