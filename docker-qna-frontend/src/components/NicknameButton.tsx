b1fbc868a26e152b0742ea7fe4849cf20f11f5fe
honeykwak <jenilove0517@naver.com>
2024-05-24 00:23:05 +0900

    ch nick kor-eng

--- docker-qna-frontend/src/components/NicknameButton.tsx
+++ docker-qna-frontend/src/components/NicknameButton.tsx
@@ -40,7 +40,7 @@ const NicknameButton = () => {
         </Button>
       ) : (
         <Chip size="lg" color="primary" variant="flat">
-           닉네임 {nickname}
+           별명 {nickname}
         </Chip>
       )}
       <Modal isOpen={isOpen} onOpenChange={onOpenChange} placement="top-center">
