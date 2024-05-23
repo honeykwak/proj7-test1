dda78ad0446be3686337c6791a3a7d26e2c44011
honeykwak <jenilove0517@naver.com>
2024-05-23 11:12:32 +0900

    edit text

--- front/front/src/components/NicknameButton.tsx
+++ front/front/src/components/NicknameButton.tsx
@@ -36,7 +36,7 @@ const NicknameButton = () => {
     <>
       {isNickname ? (
         <Button onPress={onOpen} color="primary" href="#" variant="flat">
-          질문하자?
+          질문하라고?
         </Button>
       ) : (
         <Chip size="lg" color="primary" variant="flat">
